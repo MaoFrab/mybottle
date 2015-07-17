38e70331c1c744675b81913adcc06d4d780dae9f
a.marchuk <nokysin@gmail.com>
2015-07-17 16:42:42 +0300

    TC

--- templates/main/js/parallax.js
+++ templates/main/js/parallax.js
@@ -10,3 +10,24 @@ $(document).ready(function(){
         });
     });
 });
+
+var clock;
+		
+		$(document).ready(function() {
+			var clock;
+
+			clock = $('.clock').FlipClock({
+		        clockFace: 'DailyCounter',
+		        autoStart: false,
+		        callbacks: {
+		        	stop: function() {
+		        		$('.message').html('The clock has stopped!')
+		        	}
+		        }
+		    });
+				    
+		    clock.setTime(900);
+		    clock.setCountdown(true);
+		    clock.start();
+
+		});
